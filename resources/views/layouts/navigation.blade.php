9f0997d447006ded29a8a233ddd1414e1283d8b0
Mauricio <ale.aroutin@gmail.com>
2023-07-09 20:35:57 -0400

    Fix

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
     <div class="container">
-        <a class="navbar-brand" href="{{ route('home') }}"><img src="{{ Vite::asset('public/img/logo-lecheria.png') }}" alt="..." /></a>
+        <a class="navbar-brand" href="{{ route('home') }}"><img src="{{ asset('img/logo-lecheria.png') }}" alt="..." /></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             Menu
             <i class="fas fa-bars ms-1"></i>
